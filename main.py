b7f77d051fedfef47a3dc138ddd1b9f0f9b6511e
ArekSkiba <arek.skiba77@gmail.com>
2025-01-04 21:04:05 +0100

    main.py modified fps lock

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -22,6 +26,9 @@ def main():
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
 
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
