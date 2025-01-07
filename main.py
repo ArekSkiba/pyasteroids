807b532a2b670da9c2b5ac625f9cfccb187fe38f
ArekSkiba <arek.skiba77@gmail.com>
2025-01-07 17:30:44 +0100

    player.py added and movement methods created, main.py modified to include Player object and pygame Groups

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 import pygame
-
-print("Starting asteroids!")
-
-# import everything from constants module
-# into the current file
 from constants import *
-
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
     clock = pygame.time.Clock()
     dt = 0
-    
-    while True:
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(screen, "black")
+        for obj in updatable:
+            obj.update(dt)       
+            
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-        clock.tick(60)
         dt = clock.tick(60) / 1000
-        print(dt)
 
 
 if __name__ == "__main__":
